8b5e8d394e97fa738505761d98cc3195da6bc51e
Reyes1919 <88211857+Reyes1919@users.noreply.github.com>
2021-07-30 23:16:27 -0400

    Update App.js

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ class UI {
 
 // DOM Events
 document.getElementById('product-form')
-    .addEventListener('submit', function() {
+    .addEventListener('submit', function(e) {
     const name = document.getElementById('name').value;
     const price = document.getElementById('price').value;
     const times = document.getElementById('times').value;
